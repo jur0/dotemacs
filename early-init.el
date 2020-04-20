23218d91a8f9259faa460550a5f38cd295b30963
Juraj Hlista <jurajhlista@gmail.com>
2020-04-20 20:40:30 +0200

    Initial commit

---
+++ early-init.el
@@ -0,0 +1,5 @@
+;; Do not initialise the package manager. This is done in `init.el'.
+(setq package-enable-at-startup nil)
+
+;; Do not resize the frame at this early stage.
+(setq frame-inhibit-implied-resize t)
---
+++ init.el
@@ -0,0 +1,31 @@
+(require 'package)
+
+(setq package-archives
+      '(("melpa" . "http://melpa.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")
+        ("gnu" . "http://elpa.gnu.org/packages/")))
+
+(setq-default network-security-level 'high)
+(setq-default tls-checktrust t)
+(setq-default gnutls-verify-error t)
+
+;; Initialise the packages, avoiding a re-initialisation.
+(unless (bound-and-true-p package--initialized)
+  (setq package-enable-at-startup nil)
+  (package-initialize))
+
+;; Use use-package for package management.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Configure `use-package' prior to loading it.
+(eval-and-compile
+  (setq use-package-always-ensure nil)
+  (setq use-package-always-defer nil)
+  (setq use-package-always-demand nil)
+  (setq use-package-expand-minimally nil)
+  (setq use-package-enable-imenu-support t))
+
+(eval-when-compile
+  (require 'use-package))
