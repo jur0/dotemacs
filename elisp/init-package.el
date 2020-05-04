eb78c94fe07396aa424c9e5314152e0a6336e573
Juraj Hlista <jurajhlista@gmail.com>
2020-05-04 17:34:26 +0200

    Move package config into separate file

---
+++ elisp/init-package.el
@@ -0,0 +1,44 @@
+;;; Code:
+
+;; Specify directory for storing packages.
+(setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
+(setq package-archives
+      '(("melpa" . "http://melpa.org/packages/")
+        ;; ("org" . "http://orgmode.org/elpa/")
+        ("gnu" . "http://elpa.gnu.org/packages/")))
+
+(setq-default network-security-level 'high)
+(setq-default tls-checktrust t)
+(setq-default gnutls-verify-error t)
+
+;; Initialise the packages, avoiding a re-initialisation.
+(unless (bound-and-true-p package--initialized)
+  ;; Prevent initializing twice.
+  (setq package-enable-at-startup nil)
+  (package-initialize))
+
+;; Use use-package for package management.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Configure `use-package' prior to loading it.
+(eval-and-compile
+  (setq use-package-always-ensure nil)
+  (setq use-package-always-defer nil)
+  (setq use-package-always-demand nil)
+  ;; More verbose messages from packages, could be set to t after the
+  ;; configuration works.
+  (setq use-package-expand-minimally nil)
+  (setq use-package-enable-imenu-support t))
+
+(eval-when-compile
+  (require 'use-package)
+  ;; Required by use-package for :bind.
+  (require 'bind-key))
+
+;; Required by `use-package' for :diminish.
+(use-package diminish
+  :ensure t)
+
+(provide 'init-package)
