173c1803e1e1bcf18e87c2f926bc7a591686d557
Juraj Hlista <jurajhlista@gmail.com>
2020-05-04 08:54:35 +0200

    Update early-init

--- early-init.el
+++ early-init.el
@@ -1,5 +1,18 @@
-;; Do not initialise the package manager. This is done in `init.el'.
+;;; Code:
+
+;; Defer garbage collection further back in the startup process.
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Do not initialise the package manager. This is done later.
 (setq package-enable-at-startup nil)
 
 ;; Do not resize the frame at this early stage.
 (setq frame-inhibit-implied-resize t)
+
+;; Faster to disable these here (before they've been initialized).
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(when (featurep 'ns)
+  (push '(ns-transparent-titlebar . t) default-frame-alist))
+
