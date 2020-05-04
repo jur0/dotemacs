67eb7e73fad17ea164c0318a5c014bceecb5ae15
Juraj Hlista <jurajhlista@gmail.com>
2020-05-04 21:10:18 +0200

    Add sys/guip constant

--- elisp/init-const.el
+++ elisp/init-const.el
@@ -12,8 +12,12 @@
   (eq system-type 'darwin)
   "Are we running on a Mac system?")
 
+(defconst sys/guip
+  (display-graphic-p)
+  "Are we using GUI?")
+
 (defconst sys/mac-x-p
-  (and (display-graphic-p) sys/macp)
+  (and sys/guip sys/macp)
   "Are we running under X on a Mac system?")
 
 (defconst sys/mac-cocoa-p
@@ -21,7 +25,7 @@
   "Are we running with Cocoa on a Mac system?")
 
 (defconst sys/linux-x-p
-  (and (display-graphic-p) sys/linuxp)
+  (and sys/guip sys/linuxp)
   "Are we running under X on a GNU/Linux system?")
 
 (defconst sys/rootp
