ed3974d2d2c9c30e4747201258925b307a8f3eae
Juraj Hlista <jurajhlista@gmail.com>
2020-05-05 16:18:36 +0200

    Update init-edit

--- elisp/init-edit.el
+++ elisp/init-edit.el
@@ -1,5 +1,38 @@
 ;;; Code:
 
+;; Determine the major mode to use for files that do not specify a
+;; major mode.
+(setq-default major-mode 'text-mode)
+
+;; Set default line length.
+(setq-default fill-column 80)
+
+;; Set default tab width in spaces.
+(setq-default tab-width 4)
+
+;; Permanently indent with spaces, never with TABs.
+(setq-default indent-tabs-mode nil)
+
+;; Tab first tries to indent the current line, and if the line was
+;; already indented, then try to complete the thing at point.
+(setq-default tab-always-indent 'complete)
+
+;; Add a newline automatically at the end of the file upon save.
+(setq require-final-newline t)
+
+;; Automatically revert buffers when files on disk change.
+(use-package autorevert
+  :config
+  (setq auto-revert-verbose t)
+  ;; Enable autorevert on Dired buffers.
+  (setq global-auto-revert-non-file-buffers t)
+  (global-auto-revert-mode t))
+
+;; Delete selection if you insert.
+(use-package delsel
+  :config
+  (delete-selection-mode t))
+
 ;; M-w/C-w copies/kills the whole line if region is not active.
 (use-package whole-line-or-region
   :ensure t
@@ -14,8 +47,5 @@
   :bind
   (("C-." . er/expand-region)
    ("C-," . er/contract-region)))
-
-;; Add a newline automatically at the end of the file upon save.
-(setq require-final-newline t)
 
 (provide 'init-edit)
