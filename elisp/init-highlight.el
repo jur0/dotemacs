a8610e3c85fbdf9f7a2e94d13b495eb17ca33168
Juraj Hlista <jurajhlista@gmail.com>
2020-05-17 17:52:08 +0200

    Add rainbow-mode

--- elisp/init-highlight.el
+++ elisp/init-highlight.el
@@ -5,4 +5,14 @@
   :hook
   (after-init . global-hl-line-mode))
 
+;; Sets background of a color found in text e.g. RGB. The mode is activated
+;; manually.
+(use-package rainbow-mode
+  :ensure t
+  :commands
+  (rainbow-mode)
+  :config
+  (setq rainbow-ansi-colors nil)
+  (setq rainbow-x-colors nil))
+
 (provide 'init-highlight)
