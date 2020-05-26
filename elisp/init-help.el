958ac4571667d0ec172e9e293e96d1b19eca9bf7
Juraj Hlista <jurajhlista@gmail.com>
2020-05-26 22:19:33 +0200

    Add helper function for describing keymaps

--- elisp/init-help.el
+++ elisp/init-help.el
@@ -8,6 +8,28 @@
   (setq which-key-popup-type 'minibuffer)
   (which-key-mode t))
 
+;; https://stackoverflow.com/questions/3480173/show-keys-in-emacs-keymap-value
+;; Show actual keys instead of numbers when describing a keymap.
+(defun my/describe-keymap (keymap)
+  "Describe a KEYMAP using `substitute-command-keys'."
+  (interactive
+   (icomplete-vertical-do ()
+     (list (completing-read
+            "Keymap: " (let (maps)
+                         (mapatoms (lambda (sym)
+                                     (and (boundp sym)
+                                          (keymapp (symbol-value sym))
+                                          (push sym maps))))
+                         maps)
+            nil t))))
+  (with-output-to-temp-buffer (format "*keymap: %s*" keymap)
+    (princ (format "%s\n\n" keymap))
+    (princ (substitute-command-keys (format "\\{%s}" keymap)))
+    (with-current-buffer standard-output ;; temp buffer
+      (setq help-xref-stack-item (list #'my/describe-keymap keymap)))))
+
+(define-key global-map (kbd "C-h M-k") 'my/describe-keymap)
+
 (provide 'init-help)
 
 ;;; init-help.el ends here
