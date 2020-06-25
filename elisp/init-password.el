105c3ca605bdd72f615d5a3e796dc868f4677e1c
Juraj Hlista <jurajhlista@gmail.com>
2020-06-25 20:05:08 +0200

    Add password-store

--- elisp/init-password.el
+++ elisp/init-password.el
@@ -2,12 +2,23 @@
 
 (require 'init-const)
 
-;; Interface for pass command.
+;; Major mode for pass.
 (use-package pass
   :if exec/pass
   :ensure t
   :commands
   (pass))
+
+;; Interface for common actions related to password manipulation.
+(use-package password-store
+  :if exec/pass
+  :ensure t
+  :commands
+  (password-store-copy
+   password-store-edit
+   password-store-insert)
+  :config
+  (setq password-store-time-before-clipboard-restore 30))
 
 (provide 'init-password)
 
