1c2b50ff9d376f5f39f40582161bc28fe5317e6f
Juraj Hlista <jurajhlista@gmail.com>
2020-07-05 20:54:26 +0200

    Update LSP settings

--- elisp/init-lsp.el
+++ elisp/init-lsp.el
@@ -2,13 +2,23 @@
 
 (use-package lsp-mode
   :ensure t
+  :init
+  ;; Increase max number of bytes read from subprocess in a single chunk.
+  (setq read-process-output-max (* 1024 1024)):config
   :config
   (setq lsp-keymap-prefix "C-c p")
   (setq lsp-keep-workspace-alive nil)
-  ;;(setq lsp-prefer-capf t)
+  (setq lsp-prefer-capf t)
+  (setq lsp-enable-semantic-highlighting t)
+  (setq lsp-diagnostics-modeline-scope :project)
   ;; Disable doc helper buffer.
   ;; (setq lsp-signature-auto-activate nil)
-  (setq lsp-enable-xref t))
+  :hook
+  (lsp-managed-mode . lsp-diagnostics-modeline-mode)
+  :bind
+  (:map lsp-mode-map
+        ([remap xref-find-definitions] . lsp-find-definition)
+        ([remap xref-find-references] . lsp-find-references)))
 
 (use-package lsp-ui
   :ensure t
