f11ded05607ce4a341d915171e9e11fceb393679
Juraj Hlista <jurajhlista@gmail.com>
2020-10-01 20:29:56 +0200

    Update window settings

--- elisp/init-window.el
+++ elisp/init-window.el
@@ -3,16 +3,42 @@
 (use-package window
   :init
   (setq display-buffer-alist
-        ;; TODO: setup other buffers.
-        '(("\\*\\(Backtrace\\|Warnings\\|Compile-Log\\|Messages\\)\\*"
+        '(
+          ;; TODO: Add more buffers!
+          ;; Bottom side of display.
+          ("\\*Calendar.*"
            (display-buffer-in-side-window)
-           (window-height . 0.25)
+           ;; Integer in `window-height' specifies number of lines.
+           (window-height . 20)
            (side . bottom)
-           (slot . 0)
-           ;; Make the window no-selectable by `other-window' (C-x o).
-           (window-parameters . ((no-other-window . t))))))
+           (slot . -1))
+          ("\\*ielm.*"
+           (display-buffer-in-side-window)
+           (window-height . 20)
+           (side . bottom)
+           (slot . 0))
+          (".*\\*Completions.*"
+           (display-buffer-in-side-window)
+           (window-height . 20)
+           (side . bottom)
+           (slot . 1)
+           (window-parameters . ((no-other-window . t))))
+          ("\\*\\(Backtrace\\|Warnings\\|Compile-Log\\|Messages\\)\\*"
+           (display-buffer-in-side-window)
+           (window-height . 20)
+           (side . bottom)
+           (slot . 2))
+          ;; Right side of display.
+          ("\\*\\(Help\\|Apropos|Man\\)\\*"
+           (display-buffer-in-side-window)
+           ;; Adjust the width of the window based on buffer's content width.
+           (window-width . fit-window-to-buffer)
+           (side . right)
+           (slot . 0))))
+  (setq window-resize-pixelwise t)
   ;; Resize window combinations proportionally.
   (setq window-combination-resize t)
+  (setq fit-window-to-buffer-horizontally t)
   (setq even-window-sizes 'height-only)
   (setq window-sides-vertical nil)
   :bind
@@ -51,3 +77,5 @@
   ("<s-left>" . winner-undo))
 
 (provide 'init-window)
+
+;;; init-window.el ends here
