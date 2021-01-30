261beafa04e208fd9987d21da7fc62b98861761e
Juraj Hlista <juraj@juraj.me>
2021-01-30 12:00:58 +0100

    Update early-init.el

--- early-init.el
+++ early-init.el
@@ -10,25 +10,29 @@
 (setq gc-cons-threshold most-positive-fixnum)
 (setq gc-cons-percentage 0.5)
 
-;; For every .el and .elc file loaded during start up, Emacs runs those regexps
-;; against it.
+;; For every .el and .elc file loaded during start up, Emacs runs those
+;; regexps against it.
 (setq file-name-handler-alist nil)
 
 ;; Do not load site-wide runtime initializations.
 (setq site-run-file nil)
 
-;; Do not initialise the package manager. This is done later.
+;; Do not initialise the package manager.  This is done later.
 (setq package-enable-at-startup nil)
 
 ;; Do not resize the frame at this early stage.
 (setq frame-inhibit-implied-resize t)
 
-;; Faster to disable these here (before they've been initialized).
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
-(when (featurep 'ns)
-  (push '(ns-transparent-titlebar . t) default-frame-alist))
+;; Faster to disable GUI elements here (before they've been
+;; initialized).
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+(setq use-file-dialog nil)
+(setq use-dialog-box nil)
+
+(setq inhibit-startup-screen t)
 
 (provide 'early-init)
 
